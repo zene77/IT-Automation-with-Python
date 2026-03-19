a5fb11c272580ef9c53cc4b1d2f530b657f974fc
zenes <zeneslevaite@gmail.com>
2026-03-19 09:22:55 +0100

    Actualicé disk_usage.py

--- disk_usage.py
+++ disk_usage.py
@@ -1,8 +1,1 @@
-import shutil
-
-du = shutil.disk_usage("/")
-print("Espacio total:", du.total)
-print("Usado:", du.used)
-print("Libre:", du.free)
-print('Hola, este es mi archivo disk_usage')
-
+print('archivo original')
