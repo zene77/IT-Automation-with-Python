788fa09cb164a90e6577e3a022248c254b6cfcb0
zenes <zeneslevaite@gmail.com>
2026-03-28 10:39:18 +0100

    Iterate over a list of checks and messages to avoid code duplication

--- all_checks.py
+++ all_checks.py
@@ -4,6 +4,19 @@ import shutil
 import sys
 import os
 
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
+    du = shutil.disk_usage(disk)
+    percent_free = 100 * du.free / du.total
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
 def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
@@ -26,5 +39,9 @@ def main():
 
     print("Everything ok.")
     sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
 
 
